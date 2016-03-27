232ec3dc4adeffa94f32650b9b62f6cb56997644
老雷 <leizongmin@gmail.com>
2016-03-27 12:02:00 +0800

    完成帖子更新和删除功能

--- src/middlewares/user.js
+++ src/middlewares/user.js
@@ -8,6 +8,7 @@
 
 module.exports = function (done) {
 
+
   $.checkLogin = function (req, res, next) {
 
     if (!(req.session.user && req.session.user._id)) return next(new Error('please login firstly'));
@@ -15,6 +16,22 @@ module.exports = function (done) {
     next();
 
   };
+
+
+  $.checkTopicAuthor = async function (req, res, next) {
+
+    const topic = await $.method('topic.get').call({_id: req.params.topic_id});
+    if (!topic) return next(new Error(`topic ${req.params.topic_id} does not exists`));
+
+    if (topic.authorId.toString() !== req.session.user._id.toString()) {
+      return next(new Error('access denied'));
+    }
+
+    req.topic = topic;
+    next();
+
+  };
+
 
   done();
 
