b726aac469b67553da291900086fcb006492c631
老雷 <leizongmin@gmail.com>
2016-04-06 22:44:34 +0800

    webpack dev server

--- frontend/entry.js
+++ frontend/entry.js
@@ -1,2 +1,17 @@
+import 'bootstrap-webpack';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-console.log('hello, world');
+console.log('hello. world');
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello, world</h1>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.body);
