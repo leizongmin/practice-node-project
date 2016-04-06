514ffcefe64aaf869bdeda8195cb514c9f783774
老雷 <leizongmin@gmail.com>
2016-04-06 22:55:16 +0800

    配置 react-hot

--- frontend/entry.js
+++ frontend/entry.js
@@ -1,17 +1,10 @@
 import 'bootstrap-webpack';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './App';
 
 console.log('hello. world');
 
-class App extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1>Hello, world</h1>
-      </div>
-    );
-  }
-}
+
 
 ReactDOM.render(<App />, document.body);
