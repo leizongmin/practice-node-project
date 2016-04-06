abd3e4ef2adef0221a3452c6c3c9d1e1b587383b
老雷 <leizongmin@gmail.com>
2016-04-06 23:26:25 +0800

    api client

--- src/routes/test.js
+++ src/routes/test.js
@@ -6,10 +6,12 @@
  * @author Zongmin Lei <leizongmin@gmail.com>
  */
 
+import path from 'path';
+
 module.exports = function (done) {
 
   $.router.get('/', function (req, res, next) {
-    res.end(`现在是北京时间${new Date()}`);
+    res.sendFile(path.resolve(__dirname, '../../frontend/index.html'));
   });
 
   done();
