3d5b0d0ac4af373c9ccdebcc95a7650c802f2173
老雷 <leizongmin@gmail.com>
2016-04-15 10:26:23 +0800

    fix react 热更新问题

--- frontend/entry.js
+++ frontend/entry.js
@@ -1,7 +1,18 @@
 import 'bootstrap-webpack';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Router, Route, Link, browserHistory} from 'react-router'
 import App from './App';
+import TopicDetail from './component/TopicDetail';
 
+const e = document.createElement('div');
+e.id = 'app';
+document.body.appendChild(e);
 
-ReactDOM.render(<App />, document.body);
+ReactDOM.render((
+  <Router history={browserHistory}>
+    <Route path="/" component={App}>
+      <Route path="topic/:id" component={TopicDetail}/>
+    </Route>
+  </Router>
+), e);
