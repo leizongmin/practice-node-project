d7a506d2c5fc2008b4a2085efd0b972a8d97a85a
老雷 <leizongmin@gmail.com>
2016-04-15 11:55:32 +0800

    调整Login代码缩进

--- frontend/component/Login.js
+++ frontend/component/Login.js
@@ -32,20 +32,20 @@ export default class Login extends React.Component {
     return (
       <div style={{width: 400, margin: 'auto'}}>
         <div className="panel panel-primary">
-        <div className="panel-heading">登录</div>
-          <div className="panel-body">
-          <form>
-            <div className="form-group">
-              <label htmlFor="ipt-name">用户名</label>
-              <input type="text" className="form-control" id="ipt-name" onChange={this.handleChange.bind(this, 'name')} placeholder="" />
+          <div className="panel-heading">登录</div>
+            <div className="panel-body">
+            <form>
+              <div className="form-group">
+                <label htmlFor="ipt-name">用户名</label>
+                <input type="text" className="form-control" id="ipt-name" onChange={this.handleChange.bind(this, 'name')} placeholder="" />
+              </div>
+              <div className="form-group">
+                <label htmlFor="password">密码</label>
+                <input type="password" className="form-control" id="password" onChange={this.handleChange.bind(this, 'password')} placeholder="" />
+              </div>
+              <button type="button" className="btn btn-primary" onClick={this.handleLogin.bind(this)}>登录</button>
+              </form>
             </div>
-            <div className="form-group">
-              <label htmlFor="password">密码</label>
-              <input type="password" className="form-control" id="password" onChange={this.handleChange.bind(this, 'password')} placeholder="" />
-            </div>
-            <button type="button" className="btn btn-primary" onClick={this.handleLogin.bind(this)}>登录</button>
-            </form>
-          </div>
         </div>
       </div>
     )
