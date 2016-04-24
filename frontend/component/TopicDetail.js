0d5cbffc163f738a80e501d873cde24f6026ddea
老雷 <leizongmin@gmail.com>
2016-04-24 11:58:33 +0800

    完成删除评论功能

--- frontend/component/TopicDetail.js
+++ frontend/component/TopicDetail.js
@@ -30,6 +30,17 @@ export default class TopicDetail extends React.Component {
       .catch(err => console.error(err));
   }
 
+  handleDeleteComment(cid) {
+    if (!confirm('是否删除评论？')) return;
+    deleteComment(this.state.topic._id, cid)
+      .then(comment => {
+        this.refresh();
+      })
+      .catch(err => {
+        alert(err);
+      });
+  }
+
   render() {
     const topic = this.state.topic;
     if (!topic) {
@@ -41,7 +52,9 @@ export default class TopicDetail extends React.Component {
       <div>
         <h2>{topic.title}</h2>
         <p>标签：{topic.tags.join(', ')}</p>
-        <Link to={`/topic/${topic._id}/edit`} className="btn btn-primary">编辑</Link>
+        <Link to={`/topic/${topic._id}/edit`} className="btn btn-xs btn-primary">
+          <i className="glyphicon glyphicon-edit"></i> 编辑
+        </Link>
         <hr />
         <section dangerouslySetInnerHTML={{__html: topic.html}}></section>
         <CommentEditor
@@ -62,6 +75,11 @@ export default class TopicDetail extends React.Component {
           {topic.comments.map((item, i) => {
             return (
               <li className="list-group-item" key={i}>
+                <span className="pull-right">
+                  <button className="btn btn-xs btn-danger" onClick={this.handleDeleteComment.bind(this, item._id)}>
+                    <i className="glyphicon glyphicon-trash"></i>
+                  </button>
+                </span>
                 {item.authorId}于{item.createAt}说：
                 <p dangerouslySetInnerHTML={{__html: item.html}}></p>
               </li>
