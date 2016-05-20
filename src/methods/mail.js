a5063117d93f1867506e41993affbeb36163f07f
老雷 <leizongmin@gmail.com>
2016-05-20 12:20:49 +0800

    完成发送邮件模板功能

--- src/methods/mail.js
+++ src/methods/mail.js
@@ -6,6 +6,10 @@
  * @author Zongmin Lei <leizongmin@gmail.com>
  */
 
+import fs from 'fs';
+import path from 'path';
+import rd from 'rd';
+import ejs from 'ejs';
 import nodemailer from 'nodemailer';
 
 module.exports = function (done) {
@@ -13,6 +17,14 @@ module.exports = function (done) {
 
   $.smtp = nodemailer.createTransport($.config.get('smtp'), {
     from: $.config.get('smtp.auth.user'),
+  });
+
+
+  const templates = {};
+  rd.eachFileFilterSync(path.resolve(__dirname, '../../email_templates'), /\.html$/, (f, s) => {
+    const name = path.basename(f, '.html');
+    const html = fs.readFileSync(f).toString();
+    templates[name] = ejs.compile(html);
   });
 
 
@@ -27,6 +39,26 @@ module.exports = function (done) {
 
   });
 
+
+  $.method('mail.sendTemplate').check({
+    to: {required: true},
+    subject: {required: true},
+    template: {required: true},
+  });
+  $.method('mail.sendTemplate').register(async function (params) {
+
+    const fn = templates[params.template];
+    if (!fn) throw new Error(`invalid email template "${params.template}"`);
+
+    const html = fn(params.data || {});
+
+    return $.method('mail.send').call({
+      to: params.to,
+      subject: params.subject,
+      html: html,
+    });
+
+  });
 
 
   done();
