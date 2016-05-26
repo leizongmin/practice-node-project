b21506f50e74c3defe68541ed5cde0f729a57029
老雷 <leizongmin@gmail.com>
2016-05-26 12:30:51 +0800

    单元测试 user

--- src/test.js
+++ src/test.js
@@ -11,16 +11,37 @@ import './base';
 
 
 // 清空Redis数据
-$.init.add(async function () {
-  const keys = await $.limiter.connection.keys($.config.get('limiter.redis.prefix') + '*');
-  if (keys.length > 0) {
-    await $.limiter.connection.del(keys);
-  }
+$.init.add(done => {
+  $.limiter.connection.keys($.config.get('limiter.redis.prefix') + '*', (err, keys) => {
+    if (err) return done(err);
+    if (keys.length > 0) {
+      $.limiter.connection.del(keys, done);
+    } else {
+      done();
+    }
+  });
+});
+$.init.add(done => {
+  $.captcha.connection.keys($.config.get('captcha.redis.prefix') + '*', (err, keys) => {
+    if (err) return done(err);
+    if (keys.length > 0) {
+      $.captcha.connection.del(keys, done);
+    } else {
+      done();
+    }
+  });
+});
+
+// 清空MongoDB数据库
+$.init.add(done => {
+  $.mongodb.db.dropDatabase(done);
 });
 $.init.add(async function () {
-  const keys = await $.captcha.connection.keys($.config.get('captcha.redis.prefix') + '*');
-  if (keys.length > 0) {
-    await $.captcha.connection.del(keys);
+  const data = require('./test.db');
+  for (const name in data) {
+    for (const item of data[name]) {
+      await $.mongodb.db.collection(name).save(item);
+    }
   }
 });
 
@@ -38,9 +59,7 @@ $.init((err) => {
 
 function makeRequest(method, path, params) {
   return new Promise((resolve, reject) => {
-    $.ready(err => {
-      if (err) return reject(err);
-
+    $.ready(() => {
       params = params || {};
       let req = request($.express)[method](path);
       if (method === 'get' || method === 'head') {
@@ -50,6 +69,7 @@ function makeRequest(method, path, params) {
       }
       req.expect(200).end((err, res) => {
         if (err) return reject(err);
+
         if (res.body.success) {
           resolve(res.body.result);
         } else {
--- src/test/test_user.js
+++ src/test/test_user.js
@@ -16,7 +16,7 @@ describe('user', function () {
     try {
       const ret = await request.post('/api/signup', {
         name: 'test1',
-        password: '12345678',
+        password: '123456789',
       });
       throw new Error('should throws missing parameter "email" error');
     } catch (err) {
@@ -26,10 +26,21 @@ describe('user', function () {
     {
       const ret = await request.post('/api/signup', {
         name: 'test1',
-        password: '12345678',
+        password: '123456789',
         email: 'test1@example.com',
       });
       console.log(ret);
+      expect(ret.user.name).to.equal('test1');
+      expect(ret.user.email).to.equal('test1@example.com');
+    }
+
+    {
+      const ret = await request.post('/api/login', {
+        name: 'test1',
+        password: '123456789',
+      });
+      console.log(ret);
+      expect(ret.token).to.be.a('string');
     }
 
   });
